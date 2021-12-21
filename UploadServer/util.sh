af5a20f3dcf255d32fa3934e1d2d51da275ccc3e
huanganqi <8323683+huanganqi@user.noreply.gitee.com>
2021-12-21 18:53:51 +0800

    t

--- UploadServer/util.sh
+++ UploadServer/util.sh
@@ -3,3 +3,4 @@
 mpp=`ls | grep -v "node_modules"`
 # cp -r $mpp /home/anqihuang/workspace2/CodeView/UploadServer
 cp -r $mpp /home/anqihuang/workspace/mapping-experimental/modules/data/uploadServer/frontend
+cp -r .gitignore /home/anqihuang/workspace/mapping-experimental/modules/data/uploadServer/frontend
