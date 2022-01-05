d883bd8d5522eb42d64c0f210c76fc70219cf474
haq <1079255145@qq.com>
2022-01-05 21:58:06 +0800

    remove

--- codeviewOld/src/util/interceptor/index.js
+++
@@ -1,9 +0,0 @@
-import { statistics } from 'src/util/loger'
-
-export const viewInterceptor = (fn) => {
-    return (payload) => {
-        const rv = fn(payload)
-        fn.name !== 'App' && statistics(fn.name, 'render')
-        return rv
-    }
-}
