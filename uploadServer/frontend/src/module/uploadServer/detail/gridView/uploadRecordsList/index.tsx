ab7895c76090a47b6d4753982bfd62d85b25d8d1
huanganqi <1079255145@qq.com>
2022-03-22 14:05:42 +0800

    f

--- uploadServer/frontend/src/module/uploadServer/detail/gridView/uploadRecordsList/index.tsx
+++ uploadServer/frontend/src/module/uploadServer/detail/gridView/uploadRecordsList/index.tsx
@@ -1,24 +1,24 @@
-import { FC, ReactElement } from "react";
-import { map } from "ramda";
+import { FC, ReactElement } from 'react'
+import { map } from 'ramda'
 
-import { Box, useTheme } from "@mui/material";
-import Tooltip from "@mui/material/Tooltip";
+import { Box, useTheme } from '@mui/material'
+import Tooltip from '@mui/material/Tooltip'
 
-import { useT } from "src/hooks/language";
+import { useT } from 'src/hooks/language'
 
 type Props = {
-    waitingRecords: string[];
-    uploadingRecords: string[];
-    finishedRecords: string[];
-};
+    waitingRecords: string[]
+    uploadingRecords: string[]
+    finishedRecords: string[]
+}
 
 const RecordsList: FC<{
-    data: string[];
-    type: "finished" | "waiting" | "uploading";
+    data: string[]
+    type: 'finished' | 'waiting' | 'uploading'
 }> = ({ data, type }) => {
-    const t = useT();
-    const theme = useTheme();
-    console.log(theme.palette.success.dark);
+    const t = useT()
+    const theme = useTheme()
+    console.log(theme.palette.success.dark)
 
     return (
         <Box>
@@ -27,19 +27,19 @@ const RecordsList: FC<{
                     <Box
                         key={record}
                         sx={{
-                            display: "flex",
-                            flexDirection: "row",
-                            justifyContent: "space-between",
-                            alignItems: "center",
+                            display: 'flex',
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
                             height: 30,
                             fontSize: 12,
                         }}
                     >
                         <Box
                             sx={{
-                                display: "flex",
+                                display: 'flex',
                                 flex: 1,
-                                alignItems: "center",
+                                alignItems: 'center',
                             }}
                         >
                             {record}
@@ -48,14 +48,14 @@ const RecordsList: FC<{
                             sx={{
                                 width: 60,
                                 marginLeft: 1,
-                                alignItems: "center",
-                                textAlign: "center",
-                                color: "white",
+                                alignItems: 'center',
+                                textAlign: 'center',
+                                color: 'white',
                                 borderRadius: 1,
                                 background:
-                                    type === "finished"
+                                    type === 'finished'
                                         ? theme.palette.success.dark
-                                        : type === "uploading"
+                                        : type === 'uploading'
                                         ? theme.palette.primary.dark
                                         : theme.color.grey15,
                             }}
@@ -63,16 +63,16 @@ const RecordsList: FC<{
                             {t(type)}
                         </Box>
                     </Box>
-                );
+                )
             })(data)}
         </Box>
-    );
-};
+    )
+}
 
 const UploadRecordsList: FC<Props> = props => {
-    const t = useT();
+    const t = useT()
     const { children, waitingRecords, uploadingRecords, finishedRecords } =
-        props;
+        props
 
     if (children) {
         return (
@@ -84,32 +84,32 @@ const UploadRecordsList: FC<Props> = props => {
                         <Box
                             sx={{
                                 fontSize: 14,
-                                fontWeight: "bold",
+                                fontWeight: 'bold',
                             }}
                         >
-                            {t("allRecords")}:
+                            {t('allRecords')}:
                         </Box>
                         <RecordsList
                             data={finishedRecords}
-                            type={"finished"}
+                            type={'finished'}
                         ></RecordsList>
                         <RecordsList
                             data={uploadingRecords}
-                            type={"uploading"}
+                            type={'uploading'}
                         ></RecordsList>
                         <RecordsList
                             data={waitingRecords}
-                            type={"waiting"}
+                            type={'waiting'}
                         ></RecordsList>
                     </Box>
                 }
             >
                 {children as ReactElement}
             </Tooltip>
-        );
+        )
     }
 
-    return null;
-};
+    return null
+}
 
-export default UploadRecordsList;
+export default UploadRecordsList
