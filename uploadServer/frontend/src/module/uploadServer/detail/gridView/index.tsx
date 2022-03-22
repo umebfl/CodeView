72d6da3cc7c0f928311b24e65f48e16c97d367c3
huanganqi <1079255145@qq.com>
2022-03-22 14:04:34 +0800

    f

--- uploadServer/frontend/src/module/uploadServer/detail/gridView/index.tsx
+++ uploadServer/frontend/src/module/uploadServer/detail/gridView/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react'
 
-import { map, path } from "ramda";
-import DirectionsCarFilledOutlinedIcon from "@mui/icons-material/DirectionsCarFilledOutlined";
-import SaveOutlinedIcon from "@mui/icons-material/SaveOutlined";
-import ClosedCaptionDisabledOutlinedIcon from "@mui/icons-material/ClosedCaptionDisabledOutlined";
+import { map, path } from 'ramda'
+import DirectionsCarFilledOutlinedIcon from '@mui/icons-material/DirectionsCarFilledOutlined'
+import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined'
+import ClosedCaptionDisabledOutlinedIcon from '@mui/icons-material/ClosedCaptionDisabledOutlined'
 import {
     Box,
     CircularProgress,
@@ -11,33 +11,33 @@ import {
     Theme,
     Typography,
     useTheme,
-} from "@mui/material";
+} from '@mui/material'
 
-import UploadOutlinedIcon from "@mui/icons-material/UploadOutlined";
-import CloudDoneOutlinedIcon from "@mui/icons-material/CloudDoneOutlined";
-import ReportGmailerrorredOutlinedIcon from "@mui/icons-material/ReportGmailerrorredOutlined";
-import DiscFullOutlinedIcon from "@mui/icons-material/DiscFullOutlined";
-import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
-import ErrorOutlineOutlinedIcon from "@mui/icons-material/ErrorOutlineOutlined";
-import AdjustOutlinedIcon from "@mui/icons-material/AdjustOutlined";
+import UploadOutlinedIcon from '@mui/icons-material/UploadOutlined'
+import CloudDoneOutlinedIcon from '@mui/icons-material/CloudDoneOutlined'
+import ReportGmailerrorredOutlinedIcon from '@mui/icons-material/ReportGmailerrorredOutlined'
+import DiscFullOutlinedIcon from '@mui/icons-material/DiscFullOutlined'
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline'
+import ErrorOutlineOutlinedIcon from '@mui/icons-material/ErrorOutlineOutlined'
+import AdjustOutlinedIcon from '@mui/icons-material/AdjustOutlined'
 
-import NoMoreData from "src/component/noMoreData";
-import { ViewPayloadType } from "src/module/uploadServer/detail/type";
+import NoMoreData from 'src/component/noMoreData'
+import { ViewPayloadType } from 'src/module/uploadServer/detail/type'
 import {
     diskInfoType,
     DiskStatusConfig,
     diskStatusEnum,
     slotInfoType,
-} from "src/reducer/uploadServer/type";
-import { EmptyDataMsg } from "src/component/table";
-import TooltipText from "src/module/uploadServer/detail/gridView/tooltipText";
-import { useT } from "src/hooks/language";
-import { langType } from "src/hooks/language/package/type";
-import UploadRecordsList from "src/module/uploadServer/detail/gridView/uploadRecordsList";
+} from 'src/reducer/uploadServer/type'
+import { EmptyDataMsg } from 'src/component/table'
+import TooltipText from 'src/module/uploadServer/detail/gridView/tooltipText'
+import { useT } from 'src/hooks/language'
+import { langType } from 'src/hooks/language/package/type'
+import UploadRecordsList from 'src/module/uploadServer/detail/gridView/uploadRecordsList'
 
 const iconSXProps: SxProps<Theme> = {
     fontSize: 48,
-};
+}
 
 const IconMap: Record<string, any> = {
     UploadOutlinedIcon: <UploadOutlinedIcon sx={iconSXProps} />,
@@ -49,19 +49,19 @@ const IconMap: Record<string, any> = {
     CheckCircleOutlineIcon: <CheckCircleOutlineIcon sx={iconSXProps} />,
     ErrorOutlineOutlinedIcon: <ErrorOutlineOutlinedIcon sx={iconSXProps} />,
     null: null,
-};
+}
 
 const GridView = ({ data }: ViewPayloadType) => {
-    const theme = useTheme();
+    const theme = useTheme()
 
     return (
         <Box
             sx={{
-                display: "flex",
+                display: 'flex',
                 flex: 1,
-                flexDirection: "row",
-                justifyContent: "space-around",
-                overflow: "hidden",
+                flexDirection: 'row',
+                justifyContent: 'space-around',
+                overflow: 'hidden',
                 paddingTop: 0,
                 paddingBottom: 0,
                 borderTop: theme.borderLine.lightSolid,
@@ -69,22 +69,22 @@ const GridView = ({ data }: ViewPayloadType) => {
         >
             <Box
                 sx={{
-                    display: "flex",
-                    flexDirection: "column",
+                    display: 'flex',
+                    flexDirection: 'column',
                     flex: 1,
                 }}
             >
                 {data.length ? (
                     <Box
                         sx={{
-                            display: "flex",
-                            width: "100%",
-                            justifyContent: "flex-start",
-                            flexDirection: "row",
-                            flexWrap: "wrap",
-                            overflowY: "auto",
+                            display: 'flex',
+                            width: '100%',
+                            justifyContent: 'flex-start',
+                            flexDirection: 'row',
+                            flexWrap: 'wrap',
+                            overflowY: 'auto',
                             paddingRight: 0.1,
-                            alignContent: "flex-start",
+                            alignContent: 'flex-start',
                         }}
                     >
                         {map((item: slotInfoType) => (
@@ -97,57 +97,57 @@ const GridView = ({ data }: ViewPayloadType) => {
                 )}
             </Box>
         </Box>
-    );
-};
+    )
+}
 
 const GridItem = ({ item }: { item: slotInfoType }) => {
-    const theme = useTheme();
+    const theme = useTheme()
 
     return (
         <Box
             key={item.slotId}
             sx={{
-                display: "flex",
-                color: "white",
-                height: 160,
+                display: 'flex',
+                color: 'white',
+                height: 170,
                 padding: 1,
                 minWidth: 300,
                 flexBasis: {
-                    xs: "100%",
-                    sm: "50%",
-                    md: "40%",
-                    lg: "30%",
-                    xl: "15%",
+                    xs: '100%',
+                    sm: '50%',
+                    md: '40%',
+                    lg: '30%',
+                    xl: '15%',
                 },
             }}
         >
             <Box
                 sx={{
-                    width: "100%",
+                    width: '100%',
                     border: theme.borderLine.lightSolid,
                     padding: 1,
                     borderRadius: 2,
                     backgroundColor: item.isEmpty
-                        ? "grey"
+                        ? 'grey'
                         : item.diskInfo
                         ? (path(
                               DiskStatusConfig[item.diskInfo.diskStatus].color
                           )(theme) as string)
-                        : "green",
-                    display: "flex",
-                    flexDirection: "column",
+                        : 'green',
+                    display: 'flex',
+                    flexDirection: 'column',
                 }}
             >
                 <Box
                     sx={{
-                        display: "flex",
-                        flexDirection: "row",
+                        display: 'flex',
+                        flexDirection: 'row',
                     }}
                 >
                     <Box
                         sx={{
-                            display: "flex",
-                            flexDirection: "column",
+                            display: 'flex',
+                            flexDirection: 'column',
                             flex: 1,
                         }}
                     >
@@ -198,37 +198,37 @@ const GridItem = ({ item }: { item: slotInfoType }) => {
                 <EmptySlot show={!item.diskInfo} />
             </Box>
         </Box>
-    );
-};
+    )
+}
 
 const DiskInfo = ({ data }: { data?: diskInfoType }) => {
-    const t = useT();
+    const t = useT()
 
     return data ? (
         <>
             <Box
                 sx={{
-                    display: "flex",
-                    flexDirection: "row",
-                    alignItems: "center",
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
                 }}
             >
                 <SaveOutlinedIcon />
                 <Box
                     sx={{
-                        display: "flex",
-                        flexDirection: "column",
+                        display: 'flex',
+                        flexDirection: 'column',
                     }}
                 >
                     <TooltipText
                         // showlen={20}
-                        title={`${t("disk")} ID: ${data?.diskId}`}
+                        title={`${t('disk')} ID: ${data?.diskId}`}
                     >
                         <Box
                             sx={{
-                                whiteSpace: "nowrap",
-                                textOverflow: "ellipsis",
-                                overflow: "hidden",
+                                whiteSpace: 'nowrap',
+                                textOverflow: 'ellipsis',
+                                overflow: 'hidden',
                                 marginLeft: 0.3,
                             }}
                         >
@@ -239,9 +239,9 @@ const DiskInfo = ({ data }: { data?: diskInfoType }) => {
             </Box>
             <Box
                 sx={{
-                    display: "flex",
-                    flexDirection: "row",
-                    alignItems: "center",
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
                     marginTop: 0.5,
                 }}
             >
@@ -249,28 +249,28 @@ const DiskInfo = ({ data }: { data?: diskInfoType }) => {
 
                 <TooltipText
                     showlen={20}
-                    title={data?.vehicleIds.join(",") || ""}
+                    title={data?.vehicleIds.join(',') || ''}
                 >
                     <Box
                         sx={{
-                            whiteSpace: "nowrap",
-                            textOverflow: "ellipsis",
-                            overflow: "hidden",
+                            whiteSpace: 'nowrap',
+                            textOverflow: 'ellipsis',
+                            overflow: 'hidden',
                             width: 180,
                             marginLeft: 0.3,
                         }}
                     >
                         {data?.vehicleIds.length
-                            ? data?.vehicleIds.join(",")
-                            : "-"}
+                            ? data?.vehicleIds.join(',')
+                            : '-'}
                     </Box>
                 </TooltipText>
             </Box>
             <Box
                 sx={{
-                    display: "flex",
-                    flexDirection: "row",
-                    alignItems: "center",
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
                     marginTop: 0.5,
                     fontSize: 14,
                 }}
@@ -285,8 +285,8 @@ const DiskInfo = ({ data }: { data?: diskInfoType }) => {
                     showlen={0}
                     title={
                         data?.isMounted
-                            ? `${t("mountPoint")}: ${data?.mountPoint || "-"}`
-                            : ""
+                            ? `${t('mountPoint')}: ${data?.mountPoint || '-'}`
+                            : ''
                     }
                 >
                     <Box
@@ -294,51 +294,51 @@ const DiskInfo = ({ data }: { data?: diskInfoType }) => {
                             marginLeft: 0.3,
                         }}
                     >
-                        {data?.isMounted ? t("mounted") : t("unmount")}
+                        {data?.isMounted ? t('mounted') : t('unmount')}
                     </Box>
                 </TooltipText>
             </Box>
         </>
-    ) : null;
-};
+    ) : null
+}
 
 const SlotInfo = ({ id, busId }: { id: string; busId: string }) => {
-    const t = useT();
+    const t = useT()
     return (
         <Box>
-            <TooltipText title={`${t("slot")} ID:${busId}`}>
+            <TooltipText title={`${t('slot')} ID:${busId}`}>
                 <Typography variant="h4" display="inline-block">
                     {id}
                 </Typography>
             </TooltipText>
         </Box>
-    );
-};
+    )
+}
 
 const Status = ({
     data,
     updateTimeShortStr,
 }: {
-    data?: diskStatusEnum;
-    updateTimeShortStr?: string;
+    data?: diskStatusEnum
+    updateTimeShortStr?: string
 }) => {
-    const t = useT();
+    const t = useT()
     return (
         <Box>
             {data && (
                 <Box
                     sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: "center",
-                        alignItems: "center",
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
                     }}
                 >
                     {IconMap[DiskStatusConfig[data].icon]}
 
                     <TooltipText
-                        title={`${t("updateTime")}: ${
-                            updateTimeShortStr || "-"
+                        title={`${t('updateTime')}: ${
+                            updateTimeShortStr || '-'
                         }`}
                     >
                         <Box
@@ -352,8 +352,8 @@ const Status = ({
                 </Box>
             )}
         </Box>
-    );
-};
+    )
+}
 
 const Tips = ({ data }: { data?: string }) => {
     return data ? (
@@ -363,45 +363,45 @@ const Tips = ({ data }: { data?: string }) => {
                     fontSize: 14,
                     height: 14,
                     lineHeight: 1,
-                    borderLeft: "4px solid white",
+                    borderLeft: '4px solid white',
                     paddingLeft: 1,
                     marginTop: 1,
-                    whiteSpace: "nowrap",
-                    textOverflow: "ellipsis",
-                    overflow: "hidden",
+                    whiteSpace: 'nowrap',
+                    textOverflow: 'ellipsis',
+                    overflow: 'hidden',
                 }}
             >
                 {data}
             </Box>
         </TooltipText>
-    ) : null;
-};
+    ) : null
+}
 
 const EmptySlot = ({ show }: { show: boolean }) => {
-    const theme = useTheme();
-    const t = useT();
+    const theme = useTheme()
+    const t = useT()
 
     return show ? (
         <Box
             sx={{
-                textAlign: "center",
+                textAlign: 'center',
                 marginTop: 2,
                 fontSize: 13,
                 color: theme.color.grey8,
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
             }}
         >
             <ClosedCaptionDisabledOutlinedIcon />
-            {t("hardDiskNotInserted")}
+            {t('hardDiskNotInserted')}
         </Box>
-    ) : null;
-};
+    ) : null
+}
 
 const CircularProgressWithLabel = (props: any) => {
     return (
-        <Box sx={{ position: "relative", display: "inline-flex" }}>
+        <Box sx={{ position: 'relative', display: 'inline-flex' }}>
             <CircularProgress variant="determinate" {...props} />
             <Box
                 sx={{
@@ -409,10 +409,10 @@ const CircularProgressWithLabel = (props: any) => {
                     left: 0,
                     bottom: 0,
                     right: 0,
-                    position: "absolute",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
+                    position: 'absolute',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
                 }}
             >
                 <Typography
@@ -424,7 +424,7 @@ const CircularProgressWithLabel = (props: any) => {
                 </Typography>
             </Box>
         </Box>
-    );
-};
+    )
+}
 
-export default GridView;
+export default GridView
