b1bb403baaae8a08985f379516f9226c77b9321c
huanganqi <8323683+huanganqi@user.noreply.gitee.com>
2022-06-08 11:53:50 +0800

    text

--- uploadServer/frontend/src/hooks/language/package/zh-cn.ts
+++ uploadServer/frontend/src/hooks/language/package/zh-cn.ts
@@ -81,7 +81,7 @@ const lang: langType = {
     startTime: '开始时间',
     endTime: '结束时间',
     records: '记录',
-    recordsLog: '上传日志',
+    recordsLog: '上传记录',
     operation: '操作',
     unRegDiskCanNotEditTips: '磁盘尚未上报到服务器，无法编辑！',
 }
