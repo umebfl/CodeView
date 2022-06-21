7eaa8254a2c0dd39fb308208efed130593e86b0e
huanganqi <8323683+huanganqi@user.noreply.gitee.com>
2022-06-21 10:23:16 +0800

    t

--- xr/server/index.js
+++ xr/server/index.js
@@ -5,6 +5,30 @@ const app = express()
 
 app.use(bodyParser.json())
 
+app.get('/search', (req, res) => {
+    console.log('/search', req.body)
+    res.json({
+        dir: [
+            {
+                childCount: '2',
+                lastModifiedTimestamp: 1618481963,
+                name: 'playback/byd-cn-007/',
+                sizeByte: '6133421978',
+            },
+            {
+                childCount: '2',
+                lastModifiedTimestamp: 1618481963,
+                name: 'playback/byd-cn-009/',
+                sizeByte: '6133421978',
+            },
+        ],
+        failedReason: '',
+        file: [],
+        ok: true,
+        prefix: 'playback/',
+    })
+})
+
 app.get('te', (req, res) => {
     console.log('te', req.body)
     res.json({
